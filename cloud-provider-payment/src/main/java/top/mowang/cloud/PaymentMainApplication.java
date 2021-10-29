70a8f5ea0df35593f4a596818c3d3ef47a15fb79
Xuan Li <mowangblog@qq.com>
2021-10-29 22:53:26 +0800

    init cloud-payment

---
+++ cloud-provider-payment/src/main/java/top/mowang/cloud/PaymentMainApplication.java
@@ -0,0 +1,18 @@
+package top.mowang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringCloud-Demo
+ *
+ * @author : Xuan Li
+ * @website : https://mowangblog.top
+ * @date : 2021/10/29 22:39
+ **/
+@SpringBootApplication
+public class PaymentMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMainApplication.class,args);
+    }
+}
