d60eed1c410cf930e72252f92b606b67412ebb6c
Xuan Li <mowangblog@qq.com>
2021-10-31 14:28:13 +0800

    consul的使用

---
+++ cloud-provider-payment-consul/src/main/java/top/mowang/cloud/controller/PaymentController.java
@@ -0,0 +1,29 @@
+package top.mowang.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * SpringCloud-Demo
+ *
+ * @author : Xuan Li
+ * @website : https://mowangblog.top
+ * @date : 2021/10/31 14:23
+ **/
+@RestController
+@Slf4j
+public class PaymentController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/consul")
+    public String paymentConsul()
+    {
+        return "springcloud with consul: "+serverPort+"\t   "+ UUID.randomUUID();
+    }
+}
