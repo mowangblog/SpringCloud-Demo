3e81cacab13ad5f5c6f4d38649307bde2cde9c69
Xuan Li <mowangblog@qq.com>
2021-11-01 12:44:16 +0800

    hystrix服务熔断

--- cloud-provider-payment-hystrix/src/main/java/top/mowang/cloud/PaymentHystrixMainApplication.java
+++ cloud-provider-payment-hystrix/src/main/java/top/mowang/cloud/PaymentHystrixMainApplication.java
@@ -1,10 +1,13 @@
 package top.mowang.cloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 /**
  * SpringCloud-Demo
@@ -22,4 +25,21 @@ public class PaymentHystrixMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMainApplication.class, args);
     }
+
+    /**
+     *此配置是为了服务监控而配置，与服务容错本身无关，springcloud升级后的坑
+     *ServletRegistrationBean因为springboot的默认路径不是"/hystrix.stream"，
+     *只要在自己的项目里配置上下面的servlet就可以了
+     *否则，Unable to connect to Command Metric Stream 404
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
 }
