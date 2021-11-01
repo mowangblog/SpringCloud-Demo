a7cf23da4fcd0549819c88750d3d9e90618b3df5
Xuan Li <mowangblog@qq.com>
2021-11-01 16:13:41 +0800

    gateway自定义全局过滤器

---
+++ cloud-gateway-gateway/src/main/java/top/mowang/cloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,50 @@
+package top.mowang.cloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * SpringCloud-Demo
+ *
+ * @author : Xuan Li
+ * @website : https://mowangblog.top
+ * @date : 2021/11/01 16:09
+ **/
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered
+{
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
+    {
+        log.info("***********come in MyLogGateWayFilter:  "+new Date());
+
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+
+        if(uname == null)
+        {
+            log.info("*******用户名为null，非法用户，o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        //测试语句
+        //curl http://localhost:9527/payment/lb?uname=lixuan -H "X-Request-Id:123"
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder()
+    {
+        return 0;
+    }
+}
+
